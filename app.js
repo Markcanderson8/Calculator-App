29967b0cc80c2e400d24b0e5f79c74fbd8a1d006
Mark Anderson <markcanderson@student.midlandstech.edu>
2022-05-06 09:38:37 -0400

    added new look to calculator

---
+++ app.js
@@ -0,0 +1,59 @@
+function zero() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 0);
+}
+
+function one() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 1);
+}
+
+function two() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 2);
+}
+
+function three() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 3);
+}
+
+function four() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 4);
+}
+
+function five() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 5);
+}
+
+function six() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 6);
+}
+
+function seven() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 7);
+}
+
+function eight() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 8);
+}
+
+function nine() {
+  let screen = document.getElementById("screen");
+  screen.setAttribute("value", 9);
+}
+
+function add() {
+  let num = document.getElementById("screen").value;
+  console.log(num);
+}
+
+function equals() {
+  let num = document.getElementById("screen").value;
+  console.log(num);
+}
